157bfc8f7d91545d3fc573dcefba0f3c29e21def
Bharghavbolla <bollab@mail.gvsu.edu>
2026-03-18 15:38:00 -0400

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/assignment3-bharghavbolla/',
   css: {
     preprocessorOptions: {
       scss: {
