ae5b09591e89df119d35bc714e847f285d80590e
Bharghavbolla <bollab@mail.gvsu.edu>
2026-03-18 16:07:27 -0400

    Fix base path typo

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/assignment3-bharghavbolla/',
+  base: '/assignment3-bhargavbolla/',
   css: {
     preprocessorOptions: {
       scss: {
